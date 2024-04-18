6418518ace358b67aed3f6e053f3343a44030e75
Vlad Sireanu <79110031+vladsire@users.noreply.github.com>
2024-04-18 18:50:56 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 use serde::{Deserialize, Serialize};
-use serde_json;
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct FileData {
+    /// name of the zip archive
     name: String,
+    /// list of files in the zip archive
     files: Vec<String>,
 }
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-
-    let zip_data = &args[1];
-
-    let file = File::open(zip_data)?;
+fn load_data(data_filename: &str) -> Result<Vec<FileData>, Box<dyn std::error::Error>> {
+    let file = File::open(data_filename)?;
     let reader = BufReader::new(file);
 
-    let mut zip_files: Vec<FileData> = Vec::new();
+    let mut data = Vec::new();
     for line in reader.lines() {
-        zip_files.push(serde_json::from_str(&line.unwrap()).unwrap());
+        let line = line?;
+        let line = line.trim();
+        data.push(serde_json::from_str(line)?);
     }
 
-    println!("{:?}", zip_files);
+    Ok(data)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    let data_filename = &args[1];
+    let data = load_data(&data_filename)?;
+    println!("loaded data for {} files", data.len());
 
     Ok(())
 }
